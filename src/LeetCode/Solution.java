3afab4f0d96aad6d355ff13551feb9130c71b4c2
Dheerendra Malav <dheerendra.malav@outlook.com>
2021-01-03 11:59:49 +0530

    twoSum

---
+++ src/LeetCode/Solution.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution{
+	
+	    public int[] twoSum(int[] nums, int target) {
+	        Map<Integer, Integer> prevMap = new HashMap<>();
+	        
+	        for (int i = 0; i < nums.length; i++) {
+	            int diff = target - nums[i];
+	            
+	            if (prevMap.containsKey(diff)) {
+	                return new int[]{ prevMap.get(diff), i };
+	            }
+	            prevMap.put(nums[i], i);
+	        }
+	        return new int[0]; 
+	    }
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
