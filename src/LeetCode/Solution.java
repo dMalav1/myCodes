7e69f778fbf5f5079d854f86c2e43b1bd5decf1a
Dheerendra Malav <dheerendra.malav@outlook.com>
2021-01-04 10:50:37 +0530

    Leetcode-2

--- src/LeetCode/Solution.java
+++ src/LeetCode/Solution.java
@@ -18,8 +18,36 @@ public class Solution{
 	        }
 	        return new int[0]; 
 	    }
-	}
 
+	        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	            ListNode dummy = new ListNode(0);
+	            ListNode l3 = dummy;
+	            
+	            int carry = 0;
+	            while (l1 != null || l2 != null){
+	                int l1_val = (l1 !=null) ? l1.val : 0;
+	                int l2_val = (l2 !=null) ? l2.val : 0;
+	                
+	                int sum = l1_val + l2_val + carry;
+	                carry = sum / 10;
+	                int last = sum % 10;
+	                
+	                ListNode newNode = new ListNode(last);
+	                l3.next = newNode;
+	                
+	                if (l1 != null)  l1 = l1.next;
+	                if (l2 != null)  l2 = l2.next;
+	                l3 = l3.next;
+	            }
+	            
+	            if (carry != 0){
+	                ListNode newNode = new ListNode(carry);
+	                l3.next = newNode;
+	                l3 = l3.next;
+	            }
+	            
+	            return dummy.next;
+	        }
 
 
 	public static void main(String[] args) {
