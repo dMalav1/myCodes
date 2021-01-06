220a409035f1729e4d693bbb3a581d5e843cce8a
Dheerendra Malav <dheerendra.malav@outlook.com>
2021-01-06 07:25:34 +0530

    Leetcode-4

--- src/LeetCode/Solution.java
+++ src/LeetCode/Solution.java
@@ -86,6 +86,33 @@ public class Solution{
 	            int currSubstringLength = end + 1 - start;
 	            return currSubstringLength > ans ? currSubstringLength : ans;
 	        }
+	        
+	        class Solution {
+	            public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	                int[] newNums = new int[nums1.length + nums2.length];
+	                
+	                int i = 0;
+	                int j = 0;
+	                int k = 0;
+	                
+	                while(i < nums1.length && j < nums2.length)
+	                    if(nums1[i] <= nums2[j]) 
+	                        newNums[k++] = nums1[i++];
+	                    else
+	                        newNums[k++] = nums2[j++];
+	                
+	                while(i < nums1.length)
+	                    newNums[k++] = nums1[i++];
+	                
+	                while(j < nums2.length)
+	                    newNums[k++] = nums2[j++];
+	                
+	                if(newNums.length % 2 == 0 && newNums.length > 0)
+	                    return (double)(newNums[(newNums.length - 1)/2] + newNums[(newNums.length - 1)/2 + 1])/2;
+	                else
+	                    return newNums[(newNums.length - 1)/2];
+	            }
+	        }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
